2323b350011c9699d18013c205c0fe37c1e3cfb9
keramatai <keramatai@gmail.com>
2026-09-25 16:23:53 +0800

    Refactor: Update file paths and enhance EPG and M3U8 fetching scripts

--- scripts/epg-fetch.py
+++ scripts/epg-fetch.py
@@ -10,8 +10,10 @@ from scrapers.utils import get_logger, leagues, network
 log = get_logger(Path(__file__).stem)
 
 BASE_M3U8 = Path(__file__).parent / "base.m3u8"
+EVENTS_M3U8 = Path(__file__).parent / "matches.m3u8"
+COMBINED_M3U8 = Path(__file__).parent / "vivatvsports.m3u8"
 
-EPG_FILE = Path(__file__).parent / "TV.xml"
+EPG_FILE = Path(__file__).parent / "epg.xml"
 
 EPG_URLS = [
     f"https://epgshare01.online/epgshare01/epg_ripper_{EPG_ID}.xml.gz"
--- scripts/fetch.py
+++ scripts/fetch.py
@@ -36,14 +36,12 @@ files = [
     Path(__file__).parent / f"{file}.m3u8"
     for file in (
         "base",
-        "events",
-        "TV",
-        "kodi",
+        "matches",
+        "vivatvsports"
     )
 ]
 
-BASE_FILE, EVENTS_FILE, COMBINED_FILE, KODI_FILE = files
-
+BASE_FILE, EVENTS_FILE, COMBINED_FILE = files
 
 def load_base() -> tuple[list[str], int]:
     data = BASE_FILE.read_text(encoding="utf-8")
@@ -55,55 +53,12 @@ def load_base() -> tuple[list[str], int]:
     return data.splitlines(), last_chnl_num
 
 
-def base_to_kodi(base_m3u8: list[str]) -> list[str]:
-    vlc_to_kodi = {
-        "http-referrer": "Referer",
-        "http-user-agent": "User-Agent",
-        "http-origin": "Origin",
-    }
-
-    parsed_opts = {}
-
-    out = [
-        '#EXTM3U url-tvg="https://raw.githubusercontent.com/doms9/iptv/refs/heads/default/M3U8/TV.xml"\n'
-    ]
-
-    for line in base_m3u8:
-        if line.startswith("#EXTINF:-1"):
-            out.append(line)
-
-        if line.startswith("#EXTVLCOPT:"):
-            opt = line[11:]
-
-            key, val = opt.split("=", 1)
-
-            parsed_key = vlc_to_kodi[key]
-
-            parsed_opts[parsed_key] = quote(val)
-
-        if line.startswith("http"):
-            url = line
-
-            if parsed_opts:
-                headers = "&".join(f"{k}={v}" for k, v in parsed_opts.items())
-
-                url = f"{url}|{headers}"
-
-            out.append(f"{url}\n")
-
-            parsed_opts = {}
-
-    return out
-
-
 async def main() -> None:
     log.info(f"{'=' * 10} Scraper Started {'=' * 10}")
 
     log.info("Fetching base M3U8")
 
     base_m3u8, tvg_chno = load_base()
-
-    kodi_base = base_to_kodi(base_m3u8)
 
     async with async_playwright() as p:
         try:
@@ -177,8 +132,6 @@ async def main() -> None:
 
     combined_channels: list[str] = []
 
-    kodi_combined: list[str] = []
-
     for i, (event_name, event_info) in enumerate(
         sorted(additions.items()),
         start=1,
@@ -211,15 +164,9 @@ async def main() -> None:
             source,
         ]
 
-        kodi_block: list[str] = [
-            f"{source}|Referer={quote(refer)}&Origin={quote(refer)}&User-Agent={quote(ua)}"
-        ]
-
         combined_channels.extend(["\n" + extinf_all, *vlc_block])
 
         live_events.extend(["\n" + extinf_live, *vlc_block])
-
-        kodi_combined.extend(["\n" + extinf_all, *kodi_block])
 
     COMBINED_FILE.write_text(
         "\n".join(base_m3u8 + combined_channels),
@@ -229,19 +176,12 @@ async def main() -> None:
     log.info(f"Base + Events saved to {COMBINED_FILE.resolve()}")
 
     EVENTS_FILE.write_text(
-        '#EXTM3U url-tvg="https://raw.githubusercontent.com/doms9/iptv/refs/heads/default/M3U8/TV.xml"\n'
+        '#EXTM3U url-tvg="https://raw.githubusercontent.com/keramatai/vivatvsports/refs/heads/default/scripts/epg.xml"\n'
         + "\n".join(live_events),
         encoding="utf-8",
     )
 
     log.info(f"Events saved to {EVENTS_FILE.resolve()}")
-
-    KODI_FILE.write_text(
-        "\n".join(kodi_base + kodi_combined) + "\n",
-        encoding="utf-8",
-    )
-
-    log.info(f"Kodi file saved to {KODI_FILE.resolve()}")
 
 
 if __name__ == "__main__":
